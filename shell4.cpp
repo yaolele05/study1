c727b07c06c1a26c162643f6d593ecc1f028cb73
yaolele05 <3256408162@qq.com>
2026-02-22 21:33:34 +0800

    处理前台命令和执行修改

--- shell4.cpp
+++ shell4.cpp
@@ -78,3 +78,87 @@ allline pline(const string &line)
 
     return pl;
 }
+//执行
+// 创建子进程 建立管道 重定向输入输出 执行命令 支持后台执行
+void run(const allline&pl)
+{
+    int n=pl.cmds.size();
+    vector<vector<int>>pipes(n-1,vector<int>(2));
+     vector<pid_t>pids;
+    for(int i=0;i<n;i++)
+    {
+        if(i<n-1)
+        {
+            if(pipe(pipes[i].data())==-1)
+            {
+                perror("pipe");
+                exit(1);
+            }
+        }
+        pid_t pid=fork();
+       
+        if(pid==-1)
+        {
+            perror("fork");
+            exit(1);
+        }
+         else if(pid==0)
+        {
+             //子进程
+             //恢复ctrl+c
+             signal(SIGINT,SIG_DFL);
+            //输入重定向
+            if(i==0&&pl.inf.empty()==false)
+            {
+                int fd=open(pl.inf.c_str(),O_RDONLY);
+                if(fd=-1)
+                {
+                    perror("open inf");
+                    exit(1);
+                }
+                dup2(fd,STDIN_FILENO);
+                close(fd);
+            }
+            if(i==n-1&&pl.outf.empty()==false)
+            {
+                int fd;
+                if(pl.append)
+                fd=open(pl.outf.c_str(),O_WRONLY|O_CREAT|O_APPEND,0644);
+                else
+                fd=open(pl.outf.c_str(),O_WRONLY|O_CREAT|O_TRUNC,0644);
+                if(fd==-1)
+                {
+                    perror("open outf");
+                    exit(1);
+                }
+                dup2(fd,STDOUT_FILENO);
+                close(fd);
+            }
+            //管道重定向
+          if(i>0)
+          {
+            dup2(pipes[i-1][0],STDIN_FILENO);
+          }
+          if(i<n-1)
+          {
+            dup2(pipes[i][1],STDOUT_FILENO);
+          }
+             
+        }
+
+          pids.push_back(pid);
+    }
+    for(int i=0;i<n-1;i++)
+   {
+    close(pipes[i][0]);
+    close(pipes[i][1]);
+
+   }
+   if(pl.backg==false)
+   {
+    for(auto pid:pids)
+    {
+        waitpid(pid,nullptr,0);
+    }
+   }
+}
