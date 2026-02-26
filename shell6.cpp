8501d0840d21c1344f443b4432f941389c614e52
yaolele05 <3256408162@qq.com>
2026-02-26 18:54:50 +0800

    shell验收2

---
+++ shell6.cpp
@@ -0,0 +1,313 @@
+#include <unistd.h>
+#include <bits/stdc++.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/wait.h>
+using namespace std;
+struct command
+{
+    vector<string> argv;
+};//单个命令
+struct allline
+{
+    vector<command>cmds;
+    string inf;
+    string outf; 
+    string errf;
+    bool err_append=false;
+    bool append=false;
+    bool backg=false;
+};
+void reap(int)
+{
+    //回收
+    while(waitpid(-1,nullptr,WNOHANG)>0);
+}
+//分词
+vector<string>fen(const string &line )
+{
+
+   vector<string> tokens;
+   string cur;
+   bool in_sig=false;
+   bool in_dou=false;
+   for(size_t i=0;i<line.size();i++)
+   {
+    char c=line[i];
+    if(c==' '&&in_sig==false&&in_dou==false)
+    {
+        if(cur.empty()==false)
+        {
+            tokens.push_back(cur);
+            cur.clear();
+        }
+    }
+    if(c=='\'' &&in_dou==false)
+        {
+            in_sig=!in_sig;
+            continue;
+        }
+    if(c=='"'&&in_sig==false)
+    {   
+        in_dou=!in_dou;
+        continue;
+
+    }
+    if(isspace(c)&&in_sig==false&&in_dou==false)
+    {
+        if(cur.empty()==false)
+        {
+            tokens.push_back(cur);
+            cur.clear();
+        }
+    }
+    else
+    {
+        cur.push_back(c);
+    }
+  }
+  if(cur.empty()==false)
+  {
+    tokens.push_back(cur);
+  }
+   return  tokens;
+}
+//解析
+allline pline(const string &line)
+{
+    allline pl;
+    auto tokens=fen(line);
+    command cmd;
+    for(size_t i=0;i<tokens.size();i++)
+    {
+        if(tokens[i]=="<")
+        {
+            if(i+1<tokens.size())
+            pl.inf=tokens[++i];
+
+        }
+        else if(tokens[i]==">")
+        {
+            if(i+1<tokens.size())
+            {
+                pl.outf=tokens[++i];
+               
+            }
+        }
+        else if(tokens[i]==">>")
+        {
+            pl.outf=tokens[++i];
+            pl.append=true;
+        }
+        else if(tokens[i]=="|")
+        {
+            pl.cmds.push_back(cmd);
+            cmd.argv.clear();
+        }
+        else if(tokens[i]=="&")
+        {
+            pl.backg=true;
+        }
+        else if(tokens[i]=="2>")
+        {
+            if(i+1<tokens.size())
+            {
+                pl.errf=tokens[++i];
+                
+            }
+        }
+        else if(tokens[i]=="2>>")
+        {
+            if(i+1<tokens.size())
+            {
+                pl.errf=tokens[++i];
+                pl.err_append=true;
+            }
+        }
+        else
+        {
+            string arr=tokens[i];
+            //环境变量展开(arr相当于路径)
+            if(arr.size()>1&&arr[0]=='$')
+            {
+                const char*s=getenv(arr.substr(1).c_str());
+                if(s)
+                arr=s;
+                else
+                arr="";
+
+            }
+            if(arr.size()>1&&arr[0]=='~')
+            {
+                const char *s=getenv("HOME");
+                if(s)
+                arr=s+arr.substr(1);
+            }
+            cmd.argv.push_back(arr);
+        }
+    }
+    if(cmd.argv.empty()==false)
+    pl.cmds.push_back(cmd);
+
+    return pl;
+}
+//执行
+// 创建子进程 建立管道 重定向输入输出 执行命令 支持后台执行
+void run(const allline&pl)
+{
+    int n=pl.cmds.size();
+    vector<vector<int>>pipes(n-1,vector<int>(2));
+     vector<pid_t>pids;
+    for(int i=0;i<n;i++)
+    {
+        if(i<n-1)
+        {
+            if(pipe(pipes[i].data())==-1)
+            {
+                perror("pipe");
+                exit(1);
+            }
+        }
+        pid_t pid=fork();
+       setpgid(pid,pid);
+        if(pid==-1)
+        {
+            perror("fork");
+            exit(1);
+        }
+         else if(pid==0)
+        {
+            setpgid(0,0);//子进程加入新的进程组，前台进程组由shell控制，后台进程组不受shell控制
+             signal(SIGINT,SIG_DFL);
+
+            // 输入重定向
+            if(i==0 && !pl.inf.empty()){
+                int fd=open(pl.inf.c_str(),O_RDONLY);
+                if(fd==-1){ perror("open inf"); exit(1); }
+                dup2(fd,STDIN_FILENO);
+                close(fd);
+            }
+
+            // 输出重定向
+            if(i==n-1 && !pl.outf.empty()){
+                int fd;
+                if(pl.append)
+                    fd=open(pl.outf.c_str(),O_WRONLY|O_CREAT|O_APPEND,0644);
+                else
+                    fd=open(pl.outf.c_str(),O_WRONLY|O_CREAT|O_TRUNC,0644);
+                if(fd==-1){ perror("open outf"); exit(1); }
+                dup2(fd,STDOUT_FILENO);
+                close(fd);
+            }
+            if(i==n-1 &&pl.errf.empty()==false)
+            {
+                int fd;
+                if(pl.err_append==true)
+                fd=open(pl.errf.c_str(),O_WRONLY|O_CREAT|O_APPEND,0644);
+                else
+                fd=open(pl.errf.c_str(),O_WRONLY|O_CREAT|O_TRUNC,0644);
+                if(fd==-1){perror("open errf");exit(1);}
+                dup2(fd,STDERR_FILENO);
+                close(fd);
+            }
+
+            // 管道重定向
+            if(i>0)
+                dup2(pipes[i-1][0],STDIN_FILENO);
+
+            if(i<n-1)
+                dup2(pipes[i][1],STDOUT_FILENO);
+
+            // 关闭所有 pipe
+            for(int j=0;j<n-1;j++){
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+
+            // exec
+            vector<char*> argv;
+            for(auto &s : pl.cmds[i].argv)
+                argv.push_back(const_cast<char*>(s.c_str()));
+            argv.push_back(nullptr);
+
+            execvp(argv[0],argv.data());
+            perror("execvp");
+            exit(1);
+        }
+
+       
+          pids.push_back(pid);
+    }
+        // 父进程关闭 pipe
+    for(int i=0;i<n-1;i++){
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+    }
+     if(pl.backg==false)
+     tcsetpgrp(STDIN_FILENO,pids[0]);//将前台进程组设置为第一个子进程的进程组
+     else
+    // 父进程等待
+    if(pl.backg==false){
+        for(auto pid : pids)
+            waitpid(pid,nullptr,0);
+    }
+     if(pl.backg==false)
+      tcsetpgrp(STDIN_FILENO,getpgrp());
+}
+//前台进程组 = 当前拥有终端控制权的进程组
+int main()
+{
+    signal(SIGCHLD,reap);
+    signal(SIGINT,SIG_IGN);//shell忽略ctrl+c
+   string line;
+   while(true)
+   {
+    char cwd[1024];
+    getcwd(cwd,sizeof(cwd));//cwd是当前工作目录的路径，getcwd函数将当前工作目录的路径复制到cwd数组中，sizeof(cwd)是cwd数组的大小，防止缓冲区溢出
+    cout<<cwd<<"$: ";
+    if(!getline(cin,line)) break;
+    if(line.empty()) continue;
+    auto pl=pline(line);
+    //built-in cd
+    if(pl.cmds.size()==1&&pl.backg==false)//为什么要判断后台执行？因为如果是后台执行，父进程不等待子进程结束，子进程执行cd后退出，不会影响父进程的当前目录
+    {
+        auto&cmd=pl.cmds[0];
+        if(cmd.argv[0].empty()==false)
+        {
+        if(cmd.argv[0]=="cd")
+        {
+            const char* path=cmd.argv.size()>1?cmd.argv[1].c_str():getenv("HOME");//如果没有参数，默认切换到HOME目录//getenv详细讲解？
+            if(chdir(path)==-1)
+            {
+                perror("cd");
+            }
+            continue;
+        }
+        if(cmd.argv[0]=="echo")
+        {
+            for(size_t i=1;i<cmd.argv.size();i++)
+            {
+                cout<<cmd.argv[i];
+                if(i!=cmd.argv.size()-1)
+                cout<<" ";
+            }
+            cout<<endl;
+            continue;//
+        }
+        if(cmd.argv[0]=="exit")
+        {
+            exit(0);
+        }
+        if(cmd.argv[0]=="pwd")//pwd命令显示当前工作目录
+        {
+            cout<<cwd<<endl;
+            continue;
+        }
+     }
+   }
+    run(pl);
+   }
+
+
+    return 0;
+}
