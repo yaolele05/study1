e9892a60658226f3045973b259c7cb233fa40115
yaolele05 <3256408162@qq.com>
2026-03-04 20:18:56 +0800

    endshell

--- shell6.cpp
+++ shell6.cpp
@@ -18,10 +18,22 @@ struct allline
     bool append=false;
     bool backg=false;
 };
+struct job
+{
+    int id;
+    pid_t pgid;
+    string cmdline;
+    bool stopped;
+};
+
+vector<job> jobs;
+int njid=1;//作业id从1开始
 void reap(int)
 {
+    int status;
+    pid_t pid;
     //回收
-    while(waitpid(-1,nullptr,WNOHANG)>0);
+    while(pid=waitpid(-1,&status,WNOHANG)>0);
 }
 //分词
 vector<string>fen(const string &line )
@@ -153,8 +165,11 @@ allline pline(const string &line)
 }
 //执行
 // 创建子进程 建立管道 重定向输入输出 执行命令 支持后台执行
-void run(const allline&pl)
+void run(const allline&pl,const string &line)
 {
+    pid_t pgid=0;
+    
+
     int n=pl.cmds.size();
     vector<vector<int>>pipes(n-1,vector<int>(2));
      vector<pid_t>pids;
@@ -168,8 +183,8 @@ void run(const allline&pl)
                 exit(1);
             }
         }
-        pid_t pid=fork();
-       setpgid(pid,pid);
+      pid_t pid=fork();
+     
         if(pid==-1)
         {
             perror("fork");
@@ -177,8 +192,12 @@ void run(const allline&pl)
         }
          else if(pid==0)
         {
-            setpgid(0,0);//子进程加入新的进程组，前台进程组由shell控制，后台进程组不受shell控制
+            if(pgid==0)
+            pgid=getpid();
+
+            setpgid(0,pgid);//将子进程加入到pgid进程组中
              signal(SIGINT,SIG_DFL);
+             signal(SIGTSTP,SIG_DFL);
 
             // 输入重定向
             if(i==0 && !pl.inf.empty()){
@@ -234,6 +253,14 @@ void run(const allline&pl)
             perror("execvp");
             exit(1);
         }
+        else
+        {
+            //父进程
+            if(pgid==0)
+             pgid=pid;//第一次fork的子进程的pid作为pgid
+
+             setpgid(pid,pgid);
+        }
 
        
           pids.push_back(pid);
@@ -244,36 +271,64 @@ void run(const allline&pl)
         close(pipes[i][1]);
     }
      if(pl.backg==false)
-     tcsetpgrp(STDIN_FILENO,pids[0]);//将前台进程组设置为第一个子进程的进程组
+     tcsetpgrp(STDIN_FILENO,pgid);//将前台进程组设置为第一个子进程的进程组
      else
     // 父进程等待
-    if(pl.backg==false){
-        for(auto pid : pids)
-            waitpid(pid,nullptr,0);
+    if(pl.backg==false)
+    {
+        int status;
+        while(waitpid(-pgid,&status,WUNTRACED)>0)
+         if(WIFSTOPPED(status))
+         {
+            jobs.push_back({njid++,pgid,line,1});//1表示停止
+            cout<<"["<<njid-1<<"] "<<pgid<<" stopped"<<endl;
+         }
+         tcsetpgrp(STDIN_FILENO,getpgrp());//获取当前进程的进程组id
     }
-     if(pl.backg==false)
-      tcsetpgrp(STDIN_FILENO,getpgrp());
+    else
+    {
+        jobs.push_back({njid++,pgid,line,0});
+        cout<<"["<<njid-1<<"] "<<pgid<<endl;
+    }
+
+     
 }
 //前台进程组 = 当前拥有终端控制权的进程组
 int main()
 {
+   
     signal(SIGCHLD,reap);
     signal(SIGINT,SIG_IGN);//shell忽略ctrl+c
+    signal(SIGTSTP,SIG_IGN);//shell忽略ctrl+z；
    string line;
    while(true)
    {
     char cwd[1024];
-    getcwd(cwd,sizeof(cwd));//cwd是当前工作目录的路径，getcwd函数将当前工作目录的路径复制到cwd数组中，sizeof(cwd)是cwd数组的大小，防止缓冲区溢出
+    getcwd(cwd,sizeof(cwd));//cwd是当前目录的路径，getcwd函数将当前工作目录的路径复制到cwd数组中
     cout<<cwd<<"$: ";
     if(!getline(cin,line)) break;
     if(line.empty()) continue;
     auto pl=pline(line);
-    //built-in cd
-    if(pl.cmds.size()==1&&pl.backg==false)//为什么要判断后台执行？因为如果是后台执行，父进程不等待子进程结束，子进程执行cd后退出，不会影响父进程的当前目录
+    
+    //内建  cd
+    if(pl.cmds.size()==1&&pl.backg==false)
     {
-        auto&cmd=pl.cmds[0];
+        auto &cmd = pl.cmds[0];
         if(cmd.argv[0].empty()==false)
         {
+             int stdoutnew= dup(STDOUT_FILENO);//内建命令可能搭配重定向，所以备份
+             int stderrnew = dup(STDERR_FILENO);
+             if(pl.cmds.empty()==false)
+        {
+        int fd;
+        if(pl.append)
+        fd=open(pl.outf.c_str(),O_WRONLY|O_CREAT|O_APPEND,0644);
+        else
+        fd=open(pl.outf.c_str(),O_WRONLY|O_CREAT|O_TRUNC,0644);
+        dup2(fd,STDOUT_FILENO);
+        close (fd);
+
+        }
         if(cmd.argv[0]=="cd")
         {
             const char* path=cmd.argv.size()>1?cmd.argv[1].c_str():getenv("HOME");//如果没有参数，默认切换到HOME目录//getenv详细讲解？
@@ -282,6 +337,25 @@ int main()
                 perror("cd");
             }
             continue;
+        }
+        if(cmd.argv[0]=="fg")
+        {
+            if(jobs.empty())
+            {
+                cout<<"none\n"<<endl;
+                continue;
+            }
+            job j=jobs.back();
+            jobs.pop_back();
+            tcsetpgrp(STDIN_FILENO,j.pgid);//将前台进程组设置为j.pgid
+            if(j.stopped)
+            {
+                kill(-j.pgid,SIGCONT);
+            }
+            int status;
+            waitpid(-j.pgid,&status,WUNTRACED);//W..表示停止或者结束的
+            tcsetpgrp(STDIN_FILENO,getpgrp());
+             continue;
         }
         if(cmd.argv[0]=="echo")
         {
@@ -303,11 +377,17 @@ int main()
             cout<<cwd<<endl;
             continue;
         }
+        dup2(stdoutnew,STDOUT_FILENO);
+        dup2(stderrnew,STDERR_FILENO);
+        close(stdoutnew);
+        close(stderrnew);
+    }
+    
      }
+     run(pl,line);
    }
-    run(pl);
-   }
-
-
     return 0;
-}
+ }
+
+
+   
